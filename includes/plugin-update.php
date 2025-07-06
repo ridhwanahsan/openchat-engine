d09fd9e1f0f100e132bee42020c68d9a08dcb2a8
Ridhwan Ahsan <135047921+ridhwanahsan@users.noreply.github.com>
2025-07-07 04:17:59 +0600

    v1.0.3

--- includes/plugin-update.php
+++ includes/plugin-update.php
@@ -32,7 +32,8 @@ add_filter('site_transient_update_plugins', function ($transient) {
   $license_key = get_option('openchat_license_key');
   if (!$license_key) return $transient;
 
-  $api_url = 'https://plugin-license-api.ridhwanahsan.repl.co/check-key.php?key=' . urlencode($license_key);
+  $api_url = 'https://license-autycu0ww-ridhwanahsans-projects.vercel.app/api/check-key?key=' . urlencode($license_key);
+
   $response = wp_remote_get($api_url);
 
   if (is_wp_error($response)) return $transient;
